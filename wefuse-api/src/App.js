00da9317840853ccaf4a2b31cfb3d43e5a9b8dbe
Sammy Phatlane <sammy.phatlane@altron.com>
2020-04-20 02:09:35 +0200

    Created states and attempt to fetch blog list - assign to posts

--- wefuse-api/src/App.js
+++ wefuse-api/src/App.js
@@ -1,32 +1,63 @@
-import React from 'react';
+import React, {Component} from 'react';
+//import ReactDOM from 'react-dom';
 import logo from './assets/img/logo-neurox2.png';
 import './App.css';
-import $ from 'jquery';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// console.log('wat', $);
+
+class App extends Component{
+  render(){
+    return(
+      // JSX to render goes here
+      <p>App Component</p>
+    );
+  }
+
+  state = {
+    homepage: [],
+    posts: []
+
+  }
+
+  componentDidMount(){
+    fetch('http://test.fuseclients.com/api/blog/list')
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({posts: data})
+    })
+    .catch(console.log)
+  }
 }
 
-function hamMenuFunction(x) {
-  x.classList.toggle("change");
-  $('nav').toggleClass('nav-open');
-}
+// function App() {
+//   return (
+//     <main id="main" class="blog-post" role="main">
+//       <article id="post-1" class="post" style="background-image:url('assets/img/neslihan-gunaydin-3493.jpg');">
+// 			<a href="#" title="title test" rel="bookmark" class="post-link">
+// 				<div class="row content-row post-details">
+// 					<header>
+// 						<h5>Dummy Post Category</h5>
+// 						<h1 class="post-title">Where to find ideas and inspiration for a blog post</h1>
+// 					</header>
+// 					<footer>
+// 						<div class="post-meta">
+// 							<i class="fa fa-clock-o"></i>
+// 								July 12, 2014
+// 							<i class="fa fa-user"></i>
+// 								admin
+// 							<i class="fa fa-comments-o"></i>
+// 								3 Comments											
+// 						</div>
+				
+// 					</footer>
+// 				</div>
+// 			</a>
+// 		</article>
+//     </main>
+    
+//   );
+// }
+
+
 
 export default App;
